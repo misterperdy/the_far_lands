e61d50dd0c1e41f09018b603213e3a8ff9c1ab37
Antonie Matei <antonie.matei@agvr.co>
2026-03-13 09:04:32 +0200

    take screenshot (F2) and hide UI (F1) functionality

--- Assets/Scripts/Other/_gameManager.cs
+++ Assets/Scripts/Other/_gameManager.cs
@@ -6,6 +6,8 @@ public class _gameManager : MonoBehaviour
 {
     public bool isPaused = false;
 
+    public GameObject gameGUI; //hotbar , other ui canvas to hide for screenshot
+
     //helper pause/resume functions for pause menu
     public void PauseGame() {
         Time.timeScale = 0f;
@@ -24,4 +26,33 @@ public class _gameManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
+
+    void Update() {
+        if (isPaused) return; //dont take screenshots/hide gui when paused
+
+        //listen for screenshot key (F2)
+        if (Input.GetKeyDown(KeyCode.F2)) {
+            TakeScreenshot();
+        }
+
+        //listen for hide/enable GUI key (F1)
+        if (Input.GetKeyDown(KeyCode.F1)) {
+            if(gameGUI!= null){
+                //if hide enable, if enabled hide
+                gameGUI.SetActive(!gameGUI.activeSelf);
+            }
+        }
+    }
+
+    //take screenshot and for build save in game directory,  for editor save in assets
+    public void TakeScreenshot() {
+        string timeStamp = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+        string fileName = "Screenshot_" + timeStamp + ".png";
+
+        ScreenCapture.CaptureScreenshot(fileName);
+
+        Debug.Log("screenshot taken: " + fileName);
+    }
+
+
 }
